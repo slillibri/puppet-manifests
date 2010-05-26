53d25284b918893898dd4313c2dd94a0f2fba0ed
slillibri <scott.lillibridge@gmail.com>
2010-05-26 16:15:53 -0700

    Added configuration for cassandra install

---
+++ modules/cassandra/manifests/init.pp
@@ -0,0 +1,14 @@
+class cassandra {
+  package{'cassandra': 
+    ensure => 'running',
+    require => Package['cassandra']
+    subscribe => File['cassandra_config']}
+  
+  file{'cassandra_config':
+    name => '/etc/cassandra/storage-conf.xml',
+    owner => 'root',
+    group => 'root',
+    mode => '644',
+    source => 'puppet:///cassandra/storage-conf.xml',
+    require => Package['cassandra']}
+}
