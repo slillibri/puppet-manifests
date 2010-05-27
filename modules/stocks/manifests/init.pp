293b061a26b8be97069d5dd4455a615cc40a711b
slillibri <scott.lillibridge@gmail.com>
2010-05-27 16:54:34 -0700

    Conditional run of stock scripts

--- modules/stocks/manifests/init.pp
+++ modules/stocks/manifests/init.pp
@@ -24,14 +24,10 @@ class stocks{
     require => [Class['cassandra'],Class['gems']]}
     
   exec{'/usr/local/bin/stockpublisher.rb':
-    kill_proc => 'stockpublisher',
+    unless => 'ps axf | grep -qs [s]tockpublisher',
     require => File['stockpublisher']}
   
   exec{'/usr/local/bin/stockwatcher.rb':
-    kill_proc => 'stockwatcher',
+    unless => 'ps axf | grep -qs [s]tockwatcher'
     require => File['stockwatcher']}
-
-  define kill_proc($proc) {    
-    exec{"ps axf | grep $proc | grep -v grep | awk \{'print \$1'\} | xargs kill"}
-  }
 }
