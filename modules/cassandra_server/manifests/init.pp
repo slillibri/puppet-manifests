67e13449df8f7b2d2a5dd03506e9dee5728941fa
slillibri <scott.lillibridge@gmail.com>
2010-05-27 12:06:16 -0700

    Trying to avoid name collision

---
+++ modules/cassandra_server/manifests/init.pp
@@ -0,0 +1,19 @@
+class cassandra_server {
+  service{'cassandra': 
+    require => Package['cassandra_server'],
+    ensure => 'running'}
+  
+  package{'cassandra_server':
+    name => 'cassandra'
+    ensure => 'installed'}
+  
+  file{'cassandra_config':
+    name => '/etc/cassandra/storage-conf.xml',
+    owner => 'root',
+    group => 'root',
+    mode => '644',
+    source => 'puppet:///cassandra/storage-conf.xml',
+    require => Package['cassandra_server']
+    notify => Service['cassandra']}
+    
+}
