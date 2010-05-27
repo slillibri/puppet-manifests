c944a2bc50f2cad43fb41db3cd6d2509e86fbbcc
slillibri <scott.lillibridge@gmail.com>
2010-05-26 17:12:30 -0700

    Added sshd config

---
+++ modules/sshd/manifests/init.pp
@@ -0,0 +1,13 @@
+class ssh{
+  package{'sshd':
+    require => Package['openssh-server'],
+    subscribe => File['sshd_config']}
+  
+  File{'sshd_config':
+    name => '/etc/ssh/sshd_config',
+    owner => 'root',
+    group => 'root',
+    mode => '644',
+    source => 'puppet:///sshd/sshd_config',
+    require => Package['openssh-server']}
+}
