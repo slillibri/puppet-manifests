ad9f28c5ed3ada9c06dc9b03b729c269fcefe2d4
slillibri <scott.lillibridge@gmail.com>
2010-05-27 16:50:58 -0700

    Typo

--- modules/stocks/manifests/init.pp
+++ modules/stocks/manifests/init.pp
@@ -29,9 +29,9 @@ class stocks{
   
   exec{'/usr/local/bin/stockwatcher.rb':
     kill_proc => 'stockwatcher',
-    require => File['stockwatcher'}
+    require => File['stockwatcher']}
 
   define kill_proc($proc) {    
-    exec{"ps axf | grep $proc | grep -v grep | awk {'print $1'} | xargs kill"}
+    exec{"ps axf | grep $proc | grep -v grep | awk {'print \$1'} | xargs kill"}
   }
 }
