a9b3965b922e9f7c8624290e00885ca37c8d49d6
slillibri <scott.lillibridge@gmail.com>
2010-05-28 18:50:41 -0700

    Added libmagickwand to imagemagick

--- modules/imagemagick/manifests/init.pp
+++ modules/imagemagick/manifests/init.pp
@@ -1,4 +1,6 @@
 class imagemagick{
   package{'imagemagick': ensure => 'installed'}
   package{'libmagickcore-dev': ensure => 'installed'}
+  package{'libmagickwand3': ensure => 'installed'}
+  package{'libmagickwand-dev': ensure => 'installed'}
 }
