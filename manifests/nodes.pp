ddfb017e899a6fbf4f29144bb1a2127154e1edb0
slillibri <scott.lillibridge@gmail.com>
2010-05-30 11:58:22 -0700

    added logwatch config template

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -10,12 +10,12 @@ node 'basenode' {
 }
 
 node 'li91-20.members.linode.com' inherits 'basenode' {
-	include cassandra
-	include imagemagick
-	include gems
-	
 	$tcp_packets = ['0/0:22', '127.0.0.1:9160', '74.207.254.152:8139']
   $icmp_packets = ['0/0:8', '0/0:11']
-  
+  $logwatch_mailto = 'scott.lillibridge@gmail.com'
+
+	include cassandra
+	include imagemagick
+	include gems  
   include iptables
 }
--- modules/logwatch/manifests/init.pp
+++ modules/logwatch/manifests/init.pp
@@ -1,4 +1,10 @@
 class logwatch{
   package{'logwatch':
     ensure => 'installed'}
+  
+  file{'/usr/share/default.conf/logwatch.conf':
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    content => template('logwatch.conf.erb')}
 }
