73efb23fd7e682adebc0bdcc3d8dd6a19ebc43c2
slillibri <scott.lillibridge@gmail.com>
2010-05-30 12:10:48 -0700

    logwatch template

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,16 +4,15 @@ node 'basenode' {
   include rsync
   include users
   include sshd
-  include postfix
   include hostname
 }
 
-node 'li91-20.members.linode.com' inherits 'basenode' {
+node 'li91-20.members.linode.com' {
 	$tcp_packets = ['0/0:22', '127.0.0.1:9160', '74.207.254.152:8139']
   $icmp_packets = ['0/0:8', '0/0:11']
   $logwatch_mailto = 'scott.lillibridge@gmail.com'
 
-  include logwatch
+  include basenode
 	include cassandra
 	include imagemagick
 	include gems  
