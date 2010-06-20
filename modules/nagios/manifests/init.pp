bab5b8f6c6494b48c4ed248507f3dd9f32b8a142
slillibri <scott.lillibridge@gmail.com>
2010-06-19 17:52:36 -0700

    Added nagios class

---
+++ modules/nagios/manifests/init.pp
@@ -0,0 +1,34 @@
+class nagios {
+  package{'nagios3':
+    ensure => 'installed'}
+      
+  service{'nagios3':
+    ensure => running,
+    alias  => 'nagios',
+    hasstatus => true,
+    hasrestart => true,
+    require => Package['nagios3']}
+    
+  
+  Nagios_host <<||>>
+  Nagios_service <<||>>
+  Nagios_hostextinfo <<||>>
+  
+  class target {
+    @@nagios_host{$fqdn:
+      ensure => 'present',
+      alias => $hostname,
+      address => $ipaddress,
+      use => 'generic-host'}
+    
+    @@nagios_hostextinfo {$fqdn:
+      ensure => present,
+      icon_image_alt => $operatingsystem,
+      icon_image => "base/$operatingsystem.png",
+      statusmap_image => "base/$operatingsystem.gd2"}
+      
+    @@nagios_service{"check_ping_$hostname":
+      use => 'check_ping',
+      host_name => "$fqdn"}
+  }
+}
