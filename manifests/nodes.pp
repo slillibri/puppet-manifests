962758829fa2ccca123929f45a4c3a3dad9c6e4f
slillibri <scott.lillibridge@gmail.com>
2010-06-22 18:08:47 -0700

    Added port 80 to li91-20

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,5 @@
 node 'li91-20.members.linode.com' {
-	$tcp_packets = ['0/0:22', '127.0.0.1:9160', '74.207.254.152:8139']
+	$tcp_packets = ['0/0:22', '127.0.0.1:9160', '74.207.254.152:8139', '0/0:80']
   $icmp_packets = ['0/0:8', '0/0:11']
   $logwatch_mailto = 'scott.lillibridge@gmail.com'
   
