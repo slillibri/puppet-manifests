b509341fd53eb1669b09685ae625ea35de34dbd1
slillibri <scott.lillibridge@gmail.com>
2010-06-22 18:11:28 -0700

    Renabling nagios

--- modules/nagios/manifests/init.pp
+++ modules/nagios/manifests/init.pp
@@ -19,17 +19,20 @@ class nagios {
       ensure => 'present',
       alias => $hostname,
       address => $ipaddress,
+      target => '/etc/nagios3/nagios_host.cfg',
       use => 'generic-host'}
     
     @@nagios_hostextinfo {$fqdn:
       ensure => present,
       icon_image_alt => $operatingsystem,
       icon_image => "base/$operatingsystem.png",
+      target => '/etc/nagios3/nagios_hostextinfo.cfg',
       statusmap_image => "base/$operatingsystem.gd2"}
       
     @@nagios_service{"check_ping_$hostname":
       check_command => 'check_ping',
       use => 'generic-service',
+      target => '/etc/nagios3/nagios_service.cfg',
       host_name => "$fqdn"}
   }
 }
