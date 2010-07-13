e305641161743b1154a0911fca3c9eae3597ce67
slillibri <scott.lillibridge@gmail.com>
2010-07-13 14:41:51 -0700

    Adding nginx and geo config

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,51 @@
+class nginx::base{
+  package{'nginx':
+    ensure => 'installed'}
+  
+  service{'nginx':
+    ensure => 'running',
+    subscribe => [File['nginx.conf'], File['default']]}
+  
+  file{'nginx.conf':
+    path => '/etc/nginx/nginx.conf',
+    owner => 'root', group => 'root', mode => 644,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx']}
+  
+  file{'default':
+    path => '/etc/nginx/sites-available/default',
+    owner => 'root', group => 'root', mode => 644,
+    source => 'puppet:///modules/nginx/default',
+    require => Package['nginx']}
+}
+
+class geoip::geoip inherits nginx::base{
+    file{'geoip':
+      path => '/etc/nginx/conf.d/geoip.conf',
+      owner => 'root', group => 'root', mode => 644,
+      source => 'puppet:///modules/nginx/geoip.conf',
+      require => Package['nginx'],
+      notify => Package['nginx']}
+    
+    file{'geoip_country':
+      path => '/etc/nginx/conf.d/GeoIP.dat',
+      owner => 'root', group => 'root', mode => 644,
+      source => 'puppet:///modules/nginx/GeoIP.dat',
+      require => Package['nginx']
+    }
+    
+    file{'geoip_city':
+      path => '/etc/nginx/conf.d/GeoLiteCity.dat',
+      owner => 'root', group => 'root', mode => 644,
+      source => 'puppet:///modules/nginx/GeoLiteCity.dat',
+      require => Package['nginx']
+    }
+    
+    ##How do I add the geo conf in sites-enabled? Or add a seperate site??
+    
+    file{'geo_site':
+      path => '/etc/nginx/sites-available/geo_site',
+      owner => 'root', group => 'root', mode => 644,
+      source => 'puppet://modules/nginx/geo_site',
+      require => Package['nginx']}
+}
