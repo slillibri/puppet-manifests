fcdfc8115d3950cd89fbdc4f83bb9c3ff564a582
slillibri <scott.lillibridge@gmail.com>
2010-07-14 17:56:26 -0700

    added event handler for postfix

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -38,6 +38,6 @@ class postfix{
   
   @@nagios_command{"restart_postfix":
     target => '/etc/nagios3/nagios_commands.cfg',
-    command_line => '/etc/init.d/postfix restart',
+    command_line => 'sudo /etc/init.d/postfix restart',
     command_name => 'restart_postfix'}
 }
