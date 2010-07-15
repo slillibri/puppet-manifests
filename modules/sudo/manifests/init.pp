c7d4500c051f0a3f5cae2dc89e7dcf4fea51d3d7
slillibri <scott.lillibridge@gmail.com>
2010-07-14 18:48:19 -0700

    Removed sudo files (switched to templating) File cleanup

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,19 +1,12 @@
 class sudo {
+  package  { sudo: ensure => [installed, '1.6.9p17-2'] }
 
-        package  { sudo: ensure => [installed, '1.6.9p17-2'] }
-
-        file { "/etc/sudoers":
-          owner => "root",
-          group => "root",
-          mode => 440,
-          content => template("sudoers", "$hostname"),
-          require => Package["sudo"]
-        }
-        
-        file {"local_sudoers":
-          path => "/etc/sudoers.d/$hostname",
-          owner => 'root', group => 'root', mode => 440,
-          source => "puppet:///modules/sudo/$hostname",
-          require => Package['sudo']}
+  file { "/etc/sudoers":
+    owner => "root",
+    group => "root",
+    mode => 440,
+    content => template("sudoers", "$hostname"),
+    require => Package["sudo"]
+  }
 }
 
