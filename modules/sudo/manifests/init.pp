0fbda964af817d14dfa06535ef2588557d1fff2e
slillibri <scott.lillibridge@gmail.com>
2010-07-14 18:35:09 -0700

    Moved local suders into /etc/sudoers.d

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -9,5 +9,11 @@ class sudo {
                 source => "puppet:///modules/sudo/sudoers",
                 require => Package["sudo"]
         }
+        
+        file {"local_sudoers":
+          path => "/etc/sudoers.d/$hostname",
+          owner => 'root', group => 'root', mode => 440,
+          source => "puppet:///modules/sudo/$hostname",
+          require => Package['sudo']}
 }
 
