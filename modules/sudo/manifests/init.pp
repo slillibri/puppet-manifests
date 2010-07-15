d8119f36885908b11e4c0eaedd722a267ba71c18
slillibri <scott.lillibridge@gmail.com>
2010-07-14 18:43:26 -0700

    Testing sudoers templating

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -3,11 +3,11 @@ class sudo {
         package  { sudo: ensure => [installed, '1.6.9p17-2'] }
 
         file { "/etc/sudoers":
-                owner => "root",
-                group => "root",
-                mode => 440,
-                source => "puppet:///modules/sudo/sudoers",
-                require => Package["sudo"]
+          owner => "root",
+          group => "root",
+          mode => 440,
+          content => template("sudoers", "$hostname")
+          require => Package["sudo"]
         }
         
         file {"local_sudoers":
