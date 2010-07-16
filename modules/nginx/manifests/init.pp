a303cf3c679ad924693e7cef03fc99ce3a66cc20
slillibri <scott.lillibridge@gmail.com>
2010-07-16 14:09:04 -0700

    Testing custom nagios commands

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -24,18 +24,6 @@ class nginx::base{
     target => "/etc/nagios3/conf.d/$hostname.cfg",
     service_description => 'Check web',
     host_name => "$fqdn"}
-    
-  @@nagios_service{"check_nginx_$hostname":
-    check_command => 'check_nginx',
-    use => 'generic_service',
-    target => '/etc/nagios3/conf.d/$hostname.cfg',
-    service_description => 'Check nginx',
-    host_name => "$fqdn"}
-    
-  @@nagios_command{"check_nginx":
-    command_line => '/usr/lib/nginx/plugins/check_http -H $HOSTNAME$ -I $HOSTADDRESS$ -p $nginx_port',
-    target => '/etc/nagios3/nagios_commands.cfg',
-    command_name => 'check_nginx'}
 }
 
 class nginx::geoip inherits nginx::base{
@@ -67,4 +55,17 @@ class nginx::geoip inherits nginx::base{
       source => 'puppet:///modules/nginx/geo_site',
       require => Package['nginx'],
       notify => Service['nginx']}
+      
+    @@nagios_service{"check_nginx_$hostname":
+      check_command => 'check_nginx',
+      use => 'generic_service',
+      target => '/etc/nagios3/conf.d/$hostname.cfg',
+      service_description => 'Check nginx',
+      host_name => "$fqdn"}
+
+    @@nagios_command{"check_nginx":
+      command_line => '/usr/lib/nginx/plugins/check_http -H $HOSTNAME$ -I $HOSTADDRESS$ -p $nginx_port',
+      target => '/etc/nagios3/nagios_commands.cfg',
+      command_name => 'check_nginx'}
+    
 }
