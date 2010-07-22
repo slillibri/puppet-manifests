99d75854b285ce66a9b40facc32a54703258e038
slillibri <scott.lillibridge@gmail.com>
2010-07-22 13:26:47 -0700

    Added check to nagios for queues

--- modules/rabbitmq/manifests/init.pp
+++ modules/rabbitmq/manifests/init.pp
@@ -4,4 +4,22 @@ class rabbitmq{
     
   package{'rabbitmq-server':
     ensure => 'installed'}
+  
+  file{'check_queues':
+    path => '/usr/lib/nagios/plugins/check_queues.rb',
+    owner => 'root', group => 'root', mode => 755,
+    source => 'puppet:///modules/rabbitmq/check_queues.rb'}
+  
+  @@nagios_command{'check_queues':
+    command_line => '/usr/lib/nagios/plugins/check_queues.rb',
+    target => '/etc/nagios3/nagios_commands.cfg',
+    command_name => 'check_queues',
+    require => File['check_queues']}
+  
+  @@nagios_service{'check_queues':
+    target => "/etc/nagios3/conf.d/$hostname.cfg",
+    use => 'generic-service',
+    check_command => 'check_queues',
+    service_description => 'Check RabbitMQ',
+    require => File['check_queues']}
 }
