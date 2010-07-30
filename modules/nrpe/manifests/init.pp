74980e6836c2a6f4f710c29c116a4d6b81529b26
slillibri <scott.lillibridge@gmail.com>
2010-07-30 11:49:19 -0700

    Wrong nrpe check command

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -8,21 +8,21 @@ class nrpe{
   
   @@nagios_service{"check_load_$hostname":
     target => "/etc/nagios3/conf.d/$hostname.cfg",
-    check_command => 'check_nrpe!check_load',
+    check_command => 'check_nrpe_1arg!check_load',
     use => 'generic-service',
     service_description => 'Load',
     host_name => "$fqdn"}
 
   @@nagios_service{"check_disk_$hostname":
     target => "/etc/nagios3/conf.d/$hostname.cfg",
-    check_command => 'check_nrpe!check_disk',
+    check_command => 'check_nrpe_1arg!check_disk',
     use => 'generic-service',
     service_description => 'Disk Usage',
     host_name => "$fqdn"}
 
   @@nagios_service{"check_memory_$hostname":
     target => "/etc/nagios3/conf.d/$hostname.cfg",
-    check_command => 'check_nrpe!check_memory',
+    check_command => 'check_nrpe_1arg!check_memory',
     use => 'generic-service',
     service_description => 'Check Memory',
     host_name => "$fqdn"}
