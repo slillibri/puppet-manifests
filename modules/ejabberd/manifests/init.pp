c6d9121c612a01ef3b07376834381cdbef22a12d
slillibri <scott.lillibridge@gmail.com>
2010-12-04 16:50:24 -0700

    Added check_ejabberd command for nagios

--- modules/ejabberd/manifests/init.pp
+++ modules/ejabberd/manifests/init.pp
@@ -5,7 +5,12 @@ class ejabberd {
   service{'ejabberd':
     ensure => 'running',
     require => Package['ejabberd']}
-    
+  
+  file{'jabber':
+    path => '/etc/nagios-plugns/config/jabber',
+    owner => 'root', group => 'root', mode => '644',
+    source => "puppet://modules/ejabberd/jabber"}
+  
   @@nagios_service{'check_ejabberd':
     target => "/etc/nagios3/conf.d/$hostname.cfg",
     check_command => 'check_jabber',
