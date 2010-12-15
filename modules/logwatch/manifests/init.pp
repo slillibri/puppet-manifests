1ad615c4d3bbf3f668b632033c51fbc0bb14a56e
slillibri <scott.lillibridge@gmail.com>
2010-12-15 14:11:10 -0700

    CentOS testing

--- modules/logwatch/manifests/init.pp
+++ modules/logwatch/manifests/init.pp
@@ -2,15 +2,22 @@ class logwatch{
   package{'logwatch':
     ensure => 'installed'}
   
-  package{'libsys-cpu-perl':
-    ensure => $operatingsystem ? {
-      debian => 'installed'}
-  }
-
   file{'/usr/share/logwatch/default.conf/logwatch.conf':
     owner => 'root',
     group => 'root',
     mode => 644,
     content => template('logwatch.conf.erb'),
     require => Package['logwatch']}
+    
+  case $operatingsystem {
+    debian: {include logwatch::debian}
+    centos: {include logwatch::centos}
+    default: {warning("No such operating system: $operatingsystem defined yet")}
+  }
+    class debian{
+      package{'libsys-cpu-perl':
+        ensure => 'installed'}      
+    }
+    
+    class centos{}
 }
